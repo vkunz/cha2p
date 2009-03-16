c4ce9722487b24531aea2cb65bbfe02f626ff92f
thorsten.moll <thorsten.moll@b9bd3642-118d-11de-b0d4-7fec019aa467>
2009-03-16 18:25:04 +0000

    Grundgeruest Server + Client auf wx-Basis

---
+++ trunk/Socket_Client/Socket_ClientApp.h
@@ -0,0 +1,21 @@
+/***************************************************************
+ * Name:      Socket_ClientApp.h
+ * Purpose:   Defines Application Class
+ * Author:     ()
+ * Created:   2009-03-12
+ * Copyright:  ()
+ * License:
+ **************************************************************/
+
+#ifndef SOCKET_CLIENTAPP_H
+#define SOCKET_CLIENTAPP_H
+
+#include <wx/app.h>
+
+class Socket_ClientApp : public wxApp
+{
+    public:
+        virtual bool OnInit();
+};
+
+#endif // SOCKET_CLIENTAPP_H
---
+++ trunk/Socket_Server/SocketApp.cpp
@@ -0,0 +1,33 @@
+/***************************************************************
+ * Name:      SocketApp.cpp
+ * Purpose:   Code for Application Class
+ * Author:     ()
+ * Created:   2009-03-12
+ * Copyright:  ()
+ * License:
+ **************************************************************/
+
+#include "SocketApp.h"
+
+//(*AppHeaders
+#include "SocketMain.h"
+#include <wx/image.h>
+//*)
+
+IMPLEMENT_APP(SocketApp);
+
+bool SocketApp::OnInit()
+{
+    //(*AppInitialize
+    bool wxsOK = true;
+    wxInitAllImageHandlers();
+    if ( wxsOK )
+    {
+    	SocketFrame* Frame = new SocketFrame(0);
+    	Frame->Show();
+    	SetTopWindow(Frame);
+    }
+    //*)
+    return wxsOK;
+
+}
