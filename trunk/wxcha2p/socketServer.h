42a7e7ed587bd229745c74c1635a2c942c7c32e0
thorsten.moll <thorsten.moll@b9bd3642-118d-11de-b0d4-7fec019aa467>
2009-03-19 19:46:31 +0000

    created cha2p-application; first implementation of server-socket

---
+++ trunk/wxcha2p/socketServer.h
@@ -0,0 +1,30 @@
+#ifndef SOCKET_H_INCLUDED
+#define SOCKET_H_INCLUDED
+
+#include "wx/socket.h"
+#include "wx/event.h"
+
+
+class SocketServer : public wxEvtHandler {
+public:
+    SocketServer(int port);
+
+private:
+    ~SocketServer();
+
+    void OnServerEvent(wxSocketEvent& event);
+    void OnSocketEvent(wxSocketEvent& event);
+
+    wxSocketServer *m_server;
+
+    static const long SERVER_ID;
+    static const long SOCKET_ID;
+
+//    wxMenu         *m_menuFile;  //???????????????????????
+//    wxMenuBar      *m_menuBar;  //?????????????????????
+//    bool            m_busy;  //???????????
+
+    DECLARE_EVENT_TABLE();
+};
+
+#endif // SOCKET_H_INCLUDED
