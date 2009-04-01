35d420805cd4afd236662dfacd773bc9805f0718
Valentin Kunz <vale.kunz@googlemail.com>
2009-04-01 20:14:28 +0000

    - typo fix - moc fix

--- QtCha2P/src/BuddyListFrameController.hpp
+++ QtCha2P/src/BuddyListFrameController.hpp
@@ -12,12 +12,15 @@ namespace QtCha2P
 {
 	class BuddyListFrameController : public QObject
 	{
+		// QtMeta-Object-Compiler tags
+		Q_OBJECT
+
 		private:
 			// BuddyListFrame
 			BuddyListFrame* m_frame;		
 
 			// list widget
-			QListWidget* m_buddylist;
+			QListWidget* m_buddyList;
 
 			// map to store buddy with its item
 			std::map<QString, QListWidgetItem*> m_buddies;
@@ -39,7 +42,7 @@ namespace QtCha2P
 			void addNewPrivateTab(QString nickname);
 
 		public slots:
-			void onItemDoubleClicked(QListWidgetItem*);
+			void onItemDoubleClicked(QListWidgetItem* item);
 
 	}; // class BuddyListFrameController
 } // namespace QtCha2P
